ad3d52654a22c6dbf67a055bcda85cf447f8961e
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-20 22:24:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   string_array = string.split(" ")
   reverse_string = []
   string_array.each do |word|
-    
+    reverse_string << word.reverse
   end
   reverse_string
 end
