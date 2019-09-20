ec56cda494409e6e6ba4a7d390b0e46127287336
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-20 22:24:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,6 @@ def reverse_each_word(string)
   reverse_string = []
   string_array.each do |word|
     each_letter = word.split('')
-    reverse_string.each do |letter|
-      letter.unshift(each_letter.pop)
-    end
   end
   reverse_string
 end
